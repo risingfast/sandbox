f7d14bb96569b218790edb98232a61f6040679b6
root <root@Inspiron14.localdomain>
2022-08-19 10:47:00 -0500

    update current version

--- dogAPI.js
+++ dogAPI.js
@@ -11,5 +11,24 @@ async function fStart() {
 
     const response = await fetch('https://dog.ceo/api/breeds/list/all');
     const data = await response.json();
-    console.log(data);
+    fCreateBreedList(data.message);
+}
+
+fStart();
+
+function fCreateBreedList(breedList) {
+    document.getElementById("breed-div").innerHTML = `
+        <select onchange="loadByBreed((this.value)">
+            <option>Choose a dog breed</option>
+            ${Object.keys(breedList).map(function (breed) {
+                return `<option>${breed}</option>`
+            }).join('')}
+        </select>
+    `
+}
+
+async function loadByBreed(breed) {
+    if (breed != 'Choose a dog breed') {
+        const response = await (fetch(`https//dog.ceo/api/breed/${breed}/images`));
+    }
 }
--- readme.md
+++ readme.md
@@ -33,3 +33,4 @@
     04-Aug-2022 backup current version
     06-Aug-2022 backup current version
     12-Aug-2022 backup current version
+    19-Aug-2022 backup current version
